0b0476842817da16ed47c37010f94789cb6ff585
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2024-12-16 12:52:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+switch(new Date().getDay()){
+    case 0:
+        document.getElementById("output").innerHTML="It's a weekend!!<br>";
+        break;
+    case 1:
+        document.getElementById("output").innerHTML="Wait for the weekend<br>";
+        break;
+    case 2:
+        document.getElementById("output").innerHTML="Wait for the weekend<br>";
+        break;
+    case 3:
+        document.getElementById("output").innerHTML="Wait for the weekend<br>";
+        break;
+    case 4:
+        document.getElementById("output").innerHTML="Weekend is near<br>";
+        break;
+    case 5:
+        document.getElementById("output").innerHTML="Weekend is near<br>";
+        break;
+    case 6:
+        document.getElementById("output").innerHTML="It's a weekend!!<br>";
+        break;
+    default:
+        document.getElementById("output").innerHTML="Error 404";
+}
